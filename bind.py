bbec7262390ef09b2791845c941b8fe48f963b5c
jamesaud <jamesaud@gmail.com>
2017-09-16 23:05:15 -0400

    Update bind.py

--- bind.py
+++ bind.py
@@ -9,7 +9,8 @@ def add(a, b):
     return a + b
 
 add3 = bind(add, 3)
-#print(add3(2))
+print("\nBIND 1:")
+print(add3(2))
 
 def bindable(fn):
     def binde(*args, **kwargs):
@@ -30,6 +31,7 @@ def bindable(fn):
 def add(a, b):
     return a + b
 
+print("\nBIND 2:")
 print(add.bind(3)(2))
 
 
@@ -44,5 +46,33 @@ class Dog:
     def method(self):
         print("Dog METHOD")
 
+
+print("\nBIND 3")
 method = bind(Test.call, Dog())
-#method()
+method()
+
+
+
+class Bound:
+
+    def __init__(self, fn, *a, **k):
+        self.fn = self.__bind(fn, *a, **k)
+
+    def __call__(self, *args, **kwargs):
+        return self.fn(*args, **kwargs)
+
+    def __bind(self, fn, *a, **k):
+        return lambda *n_a, **n_k: fn(*a, *n_a, **k, **n_k)
+        
+    def bind(self, *args, **kwargs):
+        self.fn = self.__bind(self.fn, *args, **kwargs)
+
+        
+b = Bound(add, 3)
+b.bind(20)
+
+print("BIND 4")
+print(b())
+
+
+    
