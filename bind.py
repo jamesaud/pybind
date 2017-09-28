65a37859ff5ae3f72fc3e1dee397146e5e117914
jamesaud <jamesaud@gmail.com>
2017-09-27 21:27:27 -0400

    Update bind.py

--- bind.py
+++ bind.py
@@ -1,5 +1,7 @@
 from functools import wraps
 
+#### FUNCTION BINDING DECORATORS
+
 # Javascript Bind in Python
 def bind(fn, *args, **kwargs):
     @wraps(fn)
@@ -39,7 +41,7 @@ print("\nBIND 2:")
 print(add.bind(3)(5))
 
 
-class Test:
+class Cat:
     def method(self):
         print("TEST METHOD")
 
@@ -52,61 +54,139 @@ class Dog:
 
 
 print("\nBIND 3")
-dog = Dog()
-method = bind(Test.call, dog)
-method()
+dog = Cat()
+cat = Cat()
+cat.call = bind(Cat.call, dog)
+cat.call()
 
 
-def bindclass(cls, **kwargs):
-    # can't take normal args, because 'this' is the first arg
-    wrapper_cls = type(cls.__name__, (cls,), {})
-    wrapper_cls.__init__ = bind(cls.__init__, **kwargs)
+#### CLASS BINDING DECORATORS
+
+def _gen_subclass(cls):
+    return type(cls.__name__, (cls,), {})
+
+def _bindedclassmethod(cls, method_name, *args, **kwargs):
+    """ Passes 'self' as the first parameter to a class function, allowing for binding of values -
+        otherwise the user may bind values to the 'self' parameter that aren't actually an instance. """
+    @wraps(getattr(cls, method_name))
+    def bind_method(self, *a, **kw):
+        return bind(getattr(cls, method_name), self, *args, *a, **kwargs, **kw)()
+    
+    return bind_method
+
+def _bindmethodtonewclass(cls, method_name, *args, **kwargs):
+    """ Creates a new class, with the given method bound with parameters"""
+    wrapper_cls = _gen_subclass(cls)
+    setattr(wrapper_cls, method_name, _bindedclassmethod(cls, method_name, *args, **kwargs))
     return wrapper_cls
 
+def bindclass(cls, *args, **kwargs):
+    """ Generates new class where the __init__ function of the class has default parameters """
+    ## _bindmethodtonewclass  functions like the below two lines
+    
+    # wrapper_cls = _gen_subclass(cls)
+    # wrapper_cls.__init__ = _bindedclassmethod(cls, "__init__", *args, **kwargs)
+    return _bindmethodtonewclass(cls, "__init__", *args, **kwargs)
+
 def bindableclass(cls):
-    if hasattr(cls, "bind"): raise AttributeError("'bind' is already defined")
+    if hasattr(cls, "bind"): raise AttributeError("'bind' is already defined in " + cls.__name__)
     cls.bind = bind(bindclass, cls)
     return cls
 
+def _makebindableclassmethod(cls, method_name):
+    method = getattr(cls, method_name)
+
+    # method.bind() will return a new class based on the method's current class
+    # bind is the same as:   lambda *a, **kw: _bindmethodtonewclass(cls, method_name, *a, **kw)
+    method.bind = bind(_bindmethodtonewclass, cls, method_name)
+    return method
+
+@bindableclass
+def bindableclassmethods(cls):
+    cls = bindableclass(cls)
+    # Remove methods that can't be properly bound to create a new class with
+    method_list = [method_name for method_name in dir(cls) if callable(getattr(cls, method_name)) and
+                   not (method_name.startswith("__") and method_name.endswith("__"))]
+        
+    # Give all user defined methods a ".bind()" method
+    for method_name in method_list:
+        _makebindableclassmethod(cls, method_name)
+
+    # Return the new class
+    return cls
+
+
+
+@bindableclassmethods
+class Quack:
+    def hello(self, y):
+        print("hello " + y)
+
+print("\nBind All Class Methods")
+NewQuack = Quack.hello.bind("world")  # should technically be a new class
+NewQuack().hello()
+
+                
 @bindableclass
 class Duck:
-    y = 2
-    def __init__(self, x):
-        print("Calling x with " + str(x))
-
+    def __init__(self, x, y="hey", z="world"):
+        print("Calling x with " + str(x) + y + z)
+        
    # Will fail becoming @bindable class if 'bind' is defined
    # def bind():
    #     pass
         
 print("\nBIND 4")
-bound_duck = bindclass(Duck, x=3)
+bound_duck = bindclass(Duck, 3)
+bound_duck = bindclass(bound_duck, "my")
 print("Hi, I'm a clone of " + bound_duck.__name__)
-bound_duck()
+bound_duck("god")
 
-bound_duck = Duck.bind(x=4)
+bound_duck = Duck.bind(4)
 print("\nHi, I'm a clone of " + bound_duck.__name__)
 bound_duck()
 
+
+### ENCAPSULATED FUNCTION BINDING
+
 class Bound:
 
-    def __init__(self, fn, *a, **k):
-        self.fn = self.__bind(fn, *a, **k)
+    def __init__(self, fn, *args, **kwargs):
+        self.fn = self.__bind(fn, *args, **kwargs)
 
     def __call__(self, *args, **kwargs):
         return self.fn(*args, **kwargs)
 
-    def __bind(self, fn, *a, **k):
-        return lambda *n_a, **n_k: fn(*a, *n_a, **k, **n_k)
+    def __bind(self, fn, *args, **kwargs):
+        return lambda *a, **kw: fn(*args, *a, **kwargs, **kw)
+
+    def __callback(self, fn, fn2):
+        @wraps(fn)
+        def callback(*args, **kwargs):
+            result = fn(*args, **kwargs)
+            return fn2(result)
+                
+        return callback
         
     def bind(self, *args, **kwargs):
         self.fn = self.__bind(self.fn, *args, **kwargs)
+        return self
 
-        
+    def add_callback(self, fn):
+        self.fn = self.__callback(self.fn, fn)
+        return self
+
 b = Bound(add, 3)
 b.bind(20)
-
 print("\nBIND 5")
 print(b())
+
+print("\nBIND 6")
+b.add_callback(str).add_callback(lambda x: x*3)
+print(b())
+
+
+
 
 
     
